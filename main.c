2cf587603766dc00ae0c16481f12d75e5e041c20
Lachlan Armitage <lachlan.armitage@uon.edu.au>
2019-04-03 08:28:15 +0000

    master: Initial draft etc

--- main.c
+++ main.c
@@ -1,3 +1,36 @@
 #include <stdio.h>
 
 int main ()
+{
+    char message [100] = "quick brown fox jumps over lazy dog\0"; 
+    int length = 100;
+    int index = 35;
+    int key = 4;
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    
+     char L ;
+
+char new_l;
+//    printf ("Enter encryption key: ");
+   // scanf ("%d", &key);
+    
+    for (index = 0; index < length; ++index)
+    {
+        
+ L = message[index]; 
+    if (L == '\0')
+        {
+        break;
+        }
+    
+ new_l = L+key;
+    
+    message[index] = new_l;
+
+    }       
+    printf("%c, %d %c, %d\n", L, L, new_l, new_l);
+    
+    printf("%s\n", message);
+    
+    return 0; 
+}
