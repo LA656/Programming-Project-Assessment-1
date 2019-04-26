0ba0b84f41180370cfa6b89afeac2e48f98d1394
Lachlan Armitage <lachlan.armitage@uon.edu.au>
2019-04-26 12:09:23 +0000

    Ceaser cipher ENCRYPTION

--- Draft.c
+++ Draft.c
@@ -1,7 +1,8 @@
+ /* ceaser cipher layout: etc (fillout later)
+*/
 
 #include <stdio.h>  // stdin & stdout macros library
 #include <string.h> //library for string length function
-#include <stdlib.h>
 
 //ceaser cipher function definition: (data type, name, argument etc)
 //storing input message in char data type and the key shift in a single int.
@@ -9,10 +10,10 @@ void ceaserCipher(char* message, int key);
 
 // now main function
 
-int main (void)
+int main ()
 {
     int key = 1; //declaring key shift as just 1
-    char message [101]; //storing input message in array max 101 characters
+    char message [200]; //storing input message in array max 200 characters
     
 //asking the user for a message to encrpt:
 printf ("enter encryption message");
@@ -23,34 +24,33 @@ fgets (message, sizeof(message), stdin);
 //showing ciphered message
 printf("ciphered message is:");
 
-ceaserCipher(message, key);
-
-    system("pause"); 
+    system ("pause"); 
 }
 //close main function and begin cypher function
 //calling cypher function
-void ceaserCipher (char* message, int key)
+void ceaserCipher (char message, int key)
     {
     int i = 0;    //variable referencing character position in string 
-    int cypherValue;
-    char cypher; //cipher variable (storedin a char data type)
-   
+    char cipher; //cipher variable (storedin a char data type)
+  
   //while loop for function body, referencing position'i' withinarray [] 
   
   /*strlen -> string length function in the header and increment -1
   gives 1 extra character in the string for terminating character until
   each character in the message has been switched for cipher.
   */
-    while (message[i] != '\0' && strlen(message)-1 > i)
-    {
-        {
+    while (message[i] != '\n' && strlen(message)-1 < i);
   /*ascii character 'a' is 97 not 0 hence -97 to compensate for each 
   ascii character/alphabetical letter. Add 97 for remainder 0 -> 26
   for cipher values*/
-        cypherValue = ((int)message[i] - 97 + key) % 26 + 97;
-        cypher = (char)(cypherValue);
-        printf ("%c", cypher);
-        i++;
-        }        
- printf('\n');
+    cipherValue = (int(message[i] - 97 + key)) mod 26 + 97;
+    cipher = (char)(cipherValue);
+    }        
+ 
 }
+
+
+
+
+
+
